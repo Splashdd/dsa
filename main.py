962a80c6d7078bd956f3d1cde01300449fbd9afa
Your Name <aleshka228@mail.com>
2022-12-21 09:23:34 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import cProfile
+
+def print_hi(name):
+
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+cProfile.run("main.py")
